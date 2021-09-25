8a29d15e8b752044b9ec557285644470859f86fa
thebowenfeng <857514.leofeng@gmail.com>
2021-09-26 03:39:32 +1000

    added index html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from celery_task import test
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     test.delay()
-    return 'Hello World!'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
