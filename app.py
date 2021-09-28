c777609e206a48366d927f83f0efa5c727c0ccb0
Fengax <857514.leofeng@gmail.com>
2021-09-29 01:02:40 +1000

    added music and model files

--- app.py
+++ app.py
@@ -1,14 +1,116 @@
-from flask import Flask, render_template
-from celery_task import test
+from flask import Flask, render_template, request, send_file
+from flask_sqlalchemy import SQLAlchemy
+import os
+from read_midi import Preprocess
+from network import RNN_Model, Bidirectional_Model
 
 app = Flask(__name__)
+app.secret_key = "hello world"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+existing_music = {
+    "moonlight1": "static/mid_files/moonlight1.mid",
+    "fur-elise": "static/mid_files/for_elise_by_beethoven.mid",
+    "alla-turca": "static/mid_files/alla-turca.mid",
+    "campanella": "static/mid_files/lacampanella.mid"
+}
+
+existing_music_tempo = {
+    "moonlight1": 2000000,
+    "fur-elise": 500000,
+    "alla-turca": 1000000,
+    "campanella": 500000
+}
+
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    test.delay()
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.String(100), nullable=False)
+    task_status = db.Column(db.String(100), nullable=True)
+
+
+@app.route('/', methods=["GET"])
+def index():
     return render_template("index.html")
 
 
+@app.route("/start_task", methods=["GET"])
+def start_task():
+    if 'existing' in request.args:
+        task_id = request.args["task_id"]
+        train = request.args['trainMusic']
+        model = request.args['model']
+        epoch = request.args['epoch']
+        length = request.args['length']
+    else:
+        # Will be implemented later, for custom music uploads
+        task_id, train, model, epoch, length = None, None, None, None, None
+
+    task = Task(task_id=task_id)
+    db.session.add(task)
+    db.session.commit()
+
+    print("start")
+    task.task_status = "Creating job"
+    db.session.commit()
+
+    if not os.path.isdir(f"outputs/{task_id}"):
+        os.makedirs(f"outputs/{task_id}")
+
+    task.task_status = "Processing"
+    db.session.commit()
+    print("processing")
+
+    processed = Preprocess(existing_music[train])
+    if model == 'gru':
+        mymodel = RNN_Model(processed)
+    else:
+        mymodel = Bidirectional_Model(processed)
+
+    task.task_status = "Training"
+    print("training")
+    db.session.commit()
+
+    mymodel.train(epoch=int(epoch))
+
+    task.task_status = "Writing"
+    print("writing")
+    db.session.commit()
+
+    mymodel.generate(num_notes=int(length), prompt=[1, 2, 3], output_file=f"outputs/{task_id}/new.mid",
+                     tempo=existing_music_tempo[train])
+
+    task.task_status = "Success"
+    db.session.commit()
+    return "success"
+
+
+@app.route('/generate/<task_id>', methods=["GET"])
+def generate(task_id):
+    return render_template("generate.html", task_id=task_id)
+
+
+@app.route("/get_status", methods=['GET'])
+def get_status():
+    task_id = request.args["task_id"]
+
+    task = Task.query.filter_by(task_id=task_id).first()
+
+    if task is None:
+        return "none"
+    else:
+        return task.task_status
+
+
+@app.route("/download/<task_id>", methods=["GET"])
+def download(task_id):
+    if os.path.isdir(f"outputs/{task_id}"):
+        return send_file(f"outputs/{task_id}/new.mid", as_attachment=True)
+    else:
+        return "<h1>This file does not exist on the server</h1>"
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- celery_task.py
+++ celery_task.py
@@ -1,9 +1,58 @@
 from celery import Celery
+import os
+from read_midi import Preprocess
+from network import RNN_Model, Bidirectional_Model
 
 app = Celery('celery_task', broker='amqps://chirboie:DJ_cJFHcT8QMDegH2JR7N5Jm2SDZbVki@shrimp.rmq.cloudamqp.com/chirboie',
              backend='db+sqlite:///celery_backend.sqlite3')
 
 
-@app.task
-def test():
-    print("bruh")
+existing_music = {
+    "moonlight1": "/static/mid_files/moonlight1.mid",
+    "fur elise": "/static/mid_files/for_else_by_beethoven.mid",
+    "alla turca": "/static/mid_files/alla-turca.mid",
+    "campanella": "/static/mid_files/lacampanella.mid"
+}
+
+existing_music_tempo = {
+    "moonlight1": 2000000,
+    "fur elise": 1000000,
+    "alla turca": 500000,
+    "campanella": 500000
+}
+
+@app.task(bind=True)
+def gen_music(self, train_filename, model, epoch, length):
+    task_id = self.request.id
+    if os.path.isdir(task_id):
+        os.makedirs(task_id)
+
+    self.update_status(
+        state='PROCESSING'
+    )
+    print("processing")
+
+    processed = Preprocess(existing_music[train_filename])
+    if model == 'gru':
+        mymodel = RNN_Model(processed)
+    else:
+        mymodel = Bidirectional_Model(processed)
+
+    self.update_status(
+        state='TRAINING'
+    )
+    print("training")
+    mymodel.train(epoch=int(epoch))
+
+    self.update_status(
+        state='WRITING'
+    )
+    print("writing")
+    mymodel.generate(num_notes=int(length), prompt=[1, 2, 3], output_file=f"{task_id}/new.mid",
+                     tempo=existing_music_tempo[train_filename])
+
+    self.update_status(
+        state='SUCCESS'
+    )
+
+    return True
